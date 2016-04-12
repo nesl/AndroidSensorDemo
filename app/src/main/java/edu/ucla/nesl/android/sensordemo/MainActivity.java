e858073e4769115dc73fa8b17a0d186f685c8cbb
Chenguang Shen <pscggeforce@gmail.com>
2016-04-12 16:51:25 -0700

    UI changes.

--- app/src/main/java/edu/ucla/nesl/android/sensordemo/MainActivity.java
+++ app/src/main/java/edu/ucla/nesl/android/sensordemo/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.kevinsawicki.http.HttpRequest;
@@ -19,15 +20,16 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
     private static final String TAG = "SensorDemo";
     private SensorManager sensorManager;
     private Sensor mAcc;
+    private TextView mAccViewX, mAccViewY, mAccViewZ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Set button listeners for start/stop
         Button button1 = (Button) findViewById(R.id.button1);
         Button button2 = (Button) findViewById(R.id.button2);
-
         if (button1 != null) {
             button1.setOnClickListener(this);
         }
@@ -35,17 +37,32 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
             button2.setOnClickListener(this);
         }
 
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        mAcc = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        // Get handle to text fields
+        mAccViewX = (TextView) findViewById(R.id.textView1);
+        mAccViewY = (TextView) findViewById(R.id.textView2);
+        mAccViewZ = (TextView) findViewById(R.id.textView3);
     }
 
     @Override
     public void onClick(View v) {
+        // Start sensor sampling
         if (v.getId() == R.id.button1) {
+            sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+            mAcc = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
             sensorManager.registerListener(this, mAcc, SensorManager.SENSOR_DELAY_UI);
         }
 
+        // Stop sensor sampling
         if (v.getId() == R.id.button2) {
+            sensorManager.unregisterListener(this);
+            sensorManager = null;
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (sensorManager != null) {
             sensorManager.unregisterListener(this);
         }
     }
@@ -59,11 +76,13 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
     private static final double FACE_DOWN_THRESHOLD = -9.5;
     private static final double FACE_UP_THRESHOLD = 9.5;
     private static final String FACE_DOWN_URL = "https://maker.ifttt.com/trigger/phone_face_down/with/key/bHOlWt_nu5xrCmTVUdoJar";
-    private static final String FACE_UP_URL = "https://maker.ifttt.com/trigger/phone_up_down/with/key/bHOlWt_nu5xrCmTVUdoJar";
+    private static final String FACE_UP_URL = "https://maker.ifttt.com/trigger/phone_face_up/with/key/bHOlWt_nu5xrCmTVUdoJar";
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        Log.i(TAG, "Acc data x=" + event.values[0] + " y=" + event.values[1] + " z=" + event.values[2]);
+        mAccViewX.setText("Acc X = " + event.values[0]);
+        mAccViewY.setText("Acc Y = " + event.values[1]);
+        mAccViewZ.setText("Acc Z = " + event.values[2]);
 
         if (!phoneFacedDown && event.values[2] < FACE_DOWN_THRESHOLD) {
             Toast.makeText(MainActivity.this, "FACE DOWN!", Toast.LENGTH_LONG).show();
@@ -79,11 +98,9 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
     }
 
     private class HTTPRequestTask extends AsyncTask<String, Void, Void>{
-
         @Override
         protected Void doInBackground(String... params) {
             try {
-
                 HttpRequest request = HttpRequest.get(params[0]);
                 if (request.ok()) {
                     Log.i(TAG, "HTTP Request OK");
